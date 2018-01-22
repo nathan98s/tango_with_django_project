b8f85e8212876ff480370071cfd60e7df5229369
Nathan <2254880S@student.gla.ac.uk>
2018-01-22 17:02:29 +0000

    third commit

--- rango/views.py
+++ rango/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
-        return HttpResponse("Rango says fuck off")
+        context_dict = {'boldmessage': "Rango, is, shite"}
+
+
+
+        return render(request, 'rango/index.html', context = context_dict)
 
 # Create your views here.
 
