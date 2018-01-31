7b90673f716353f244867371bb8f0a0380f03c44
Nathan <2254880S@student.gla.ac.uk>
2018-01-31 14:21:41 +0000

    tenth commit

--- rango/views.py
+++ rango/views.py
@@ -4,26 +4,6 @@ from rango.models import Category
 from rango.models import Page
 
 
-def index(request):
-
-        
-        
-        category_list = Category.objects.order_by('-likes')[:5]
-        context_dict = {'categories':category_list}
-
-
-        mostViewed_list = Page.objects.order_by('-views') [:5]
-        mostViewed_dict = {'mostViewed':mostViewed_list}
-
-
-
-        return render(request, 'rango/index.html', context_dict, mostViewed_dict)
-
-def about(request):
-
-        return render(request, 'rango/about.html')
-
-
 def show_category(request, category_name_slug):
         
         context_dict = {}
@@ -38,9 +18,48 @@ def show_category(request, category_name_slug):
                 context_dict['category'] = category
                 
         except Category.DoesNotExist:
+
+               
                 
                 context_dict['category'] = None
                 context_dict['pages'] = None
                 
-        return render(request, 'rango/category.html')
+        return render(request, 'rango/category.html', context_dict)
+
+
+def most_viewed(request, page_name_slug):
+        
+        context_dict = {}
+
+        try:
+                pages = Page.objects.get(slug=page_name_slug)
+
+                context_dict['pages'] = pages
+                context_dict['views'] = view
+        except: Page/DoesNotExist:
+                context_dict['pages'] = None
+                context_dict['views'] = None
+
+        return render(request, 'rango
+def index(request):
+
+        
+        
+        category_list = Category.objects.order_by('-likes')[:5]
+        context_dict = {'categories':category_list}
+
+
+        mostViewed_list = Page.objects.order_by('-views')[:5]
+        mostViewed_dict = {'mostViewed':mostViewed_list}
+
+
+
+        return render(request, 'rango/index.html', context_dict, mostViewed_dict)
+
+def about(request):
+
+        return render(request, 'rango/about.html')
+
+
+
 
